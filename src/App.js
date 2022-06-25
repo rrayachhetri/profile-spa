7b9cdd427284fe24443660c7341763fc22486d10
rrayachhetri <rrayachhetri@gmail.com>
2022-06-25 16:55:19 -0700

    gallery comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 import './App.css';
 
 function App() {
@@ -8,8 +9,11 @@ function App() {
     <div>
       <Nav />
       <main>
+
+        <Gallery />
+
         <About />
-        
+
       </main>
 
     </div>
