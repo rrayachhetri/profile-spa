530e2c480f3b9e78534d83c57a501e922a9fb88a
rrayachhetri <rrayachhetri@gmail.com>
2022-06-25 12:27:31 -0700

    React test librariers

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -3,17 +3,19 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from "..";
 afterEach(cleanup);
-describe('About component', ()=> {
+//  we won't have any leftover memory data that could give us false results.
+describe('About component', () => {
     // first test
-    it('renders', ()=> {
+    it('renders', () => {
         render(<About />);
     });
- 
+
     //second test
-    it('matches snapshot DOM node structure', ()=> {
+    it('matches snapshot DOM node structure', () => {
         // render About
-    })
-    const { asFragment } = render(<About />);
-    expect(asFragment()).toMatchSnapshot();
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+    //expect function with a matcher to assert something about a value
 
 })
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,16 +18,16 @@ const Nav = () => {
         console.log(`${name} clicked!`)
     }
     return (
-        <header >
+        <header className="flex-row px-1" >
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera">📸</span>Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about" onClick={()=>categorySelected()}>
+                        <a data-testid="about" href="#about" onClick={()=>categorySelected()}>
                             About me
                         </a>
                     </li>
