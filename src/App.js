f5a47473e62e32f347930a730ec412b72e39e685
rrayachhetri <rrayachhetri@gmail.com>
2022-07-01 01:08:02 -0700

    useEffect to re-render the changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Nav />
-      <main>
+  const [categories] = useState([
 
-        <Gallery />
-        
-        <About />
-        
-      </main>
+    {
+      name: "commercial",
+      description: "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    { name: "landscape", description: "Fields, framhouse, waterfalls, and the beauty of nature" },
+    
+  ]);
 
-    </div>
-  );
+  const [CurrentCategory, setCurrentCategory] = useState(categories[0]);
+
+return (
+  <div>
+    <Nav 
+    categories={categories}
+    setCurrentCategory={setCurrentCategory}
+    CurrentCategory={CurrentCategory}
+    
+  />
+    <main>
+
+      <Gallery />
+
+      <About />
+
+    </main>
+
+  </div>
+);
 }
 export default App;
